849e3803178d09eefd51ef45779cd688372e22f1
ZeZombini <zezombini@gmail.com>
2018-08-19 20:19:47 +0200

    Add old mysql and mariadb compatibility

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
         \Carbon\Carbon::setUTF8(true);
         \Carbon\Carbon::setLocale(config('app.locale'));
         setlocale(LC_TIME, config('app.locale'));
