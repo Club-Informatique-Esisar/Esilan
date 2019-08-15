e2d5fe885a9b8466020227420b056963261e2f31
ZeZombini <zezombini@gmail.com>
2019-08-15 10:19:56 +0300

    Add menubar

--- src/resources/views/esilan/show.blade.php
+++ src/resources/views/esilan/show.blade.php
@@ -4,6 +4,31 @@
 - {{$esilan->name}}
 @endsection
 
+
+{{-- Navigation bar --}}
+@section('menuBar')
+        <ul>
+            @if($page == "home")
+            <li class="active"><a href="?page=home">Accueil</a></li>
+            @else
+            <li><a href="?page=home">Accueil</a></li>
+            @endif
+
+            @if($page == "register")
+            <li class="active"><a href="?page=register">Inscription</a></li>
+            @else
+            <li><a href="?page=register">Inscription</a></li>
+            @endif
+
+            @if($page == "tournament")
+            <li class="active"><a href="?page=tournament">Tournois</a></li>
+            @else
+            <li><a href="?page=tournament">Tournois</a></li>
+            @endif
+        </ul>
+@endsection
+
+
 @section('leftContent')
         <h2>Description</h2>
         <article class="clearfix">
