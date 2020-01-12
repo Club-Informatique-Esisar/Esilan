a8af21cfc3b4ece468cdbdb86c77fd9f879d8715
ZeZombini <zezombini@gmail.com>
2020-01-12 14:35:24 +0100

    Add tournament tab on esilan page

--- src/resources/views/esilan/show.blade.php
+++ src/resources/views/esilan/show.blade.php
@@ -63,4 +63,44 @@
         </div>
         @endforeach
 @endsection
+
+{{-- PAGE == TOURNAMENT --}}
+@elseif($page == "tournament")
+@section('leftContent')
+        <h2>Liste des tournois</h2>
+
+        <div class="tournament-list">
+          @foreach($esilan->tournaments as $t)
+          <div class="tournament-item">
+            <figure>
+              <img src="{{ asset($t->fullImgPathOrDefault()) }}">
+            </figure>
+            <div class="text">
+              <h3>{{ $t->name }}<span class="time italic"> de {{ $t->beginDate->formatLocalized('%H:%M') }} - {{ $t->endDate->formatLocalized('%H:%M')  }}</span></h3>
+              <p class="small italic">{{ $t->game->name }}</p>
+
+              @guest
+              <span class="bottom"><i>Vous devez être connecté pour vous inscrire au tournoi</i></span>
+              @else
+                @if(Auth::user()->isRegisterToEsilan($esilan->id))
+                  @if(Auth::user()->isRegisterToTournament($t->id))
+                  <div class="bottom">
+                    <p>Vous êtes inscrit à ce tournoi</p>
+                    <a class="btn btn-grey">Modifier mon inscription</a>
+                  </div>
+                  @else
+                    @if(Auth::user()->canRegisterToTournament($t->id))
+                      <a class="btn btn-grey bottom">S'inscrire</a>
+                    @else
+                    <span class="bottom"><i>Votre place d'Esilan ne vous permet pas de vous inscrire à ce tournoi.</i></span>
+                    @endif {{-- endif canRegisterToTournament --}}
+                  @endif {{-- endif isRegisterToTournament --}}
+                @endif
+              @endguest
+            </div>
+          </div>
+          @endforeach
+        </div>
+@endsection
+
 @endif
