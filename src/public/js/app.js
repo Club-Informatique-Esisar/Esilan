608a911985f0bd516b25e24569aeff19bdd0dcb6
ZeZombini <zezombini@gmail.com>
2020-01-12 10:23:07 +0100

    Add pop up

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -30,7 +30,20 @@ $(document).ready(function() {
     if ( typeof( CKEDITOR ) != "undefined" ) CKEDITOR.replace( 'inputDesc' );
 
     // TODO: Why not vanilla js form img-fullable
+    // Popup
+    // Todo : Use class ?
+    var mpopup = document.getElementById('mpopupBox');
+    var mpLink = document.getElementById("mpopupLink");
+    var close = document.getElementById("mpopupClose");
 
+    mpLink.onclick = function() { mpopup.style.display = "block"; }
+    close.onclick = function() {  mpopup.style.display = "none";  }
+    // close the mPopup when user clicks outside of the box
+    window.onclick = function(event) {
+        if (event.target == mpopup) {
+            mpopup.style.display = "none";
+        }
+    }
 });
 
 
